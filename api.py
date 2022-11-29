703610e4c7aa9f6c737bf5960b059f8be54f89cb
Vondy <gilenogong@hotmail.com>
2022-11-29 10:08:50 -0300

    Added UI and dodge feature

--- api.py
+++ api.py
@@ -1,36 +1,48 @@
 import urllib3
 from requests.auth import HTTPBasicAuth
-from psutil import process_iter
+import psutil
 import requests
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
+def get_args():
+    for e in psutil.process_iter():
+        if e.name() in ["LeagueClientUx", "LeagueClientUx.exe"]:
+            lcu_process = e
+            break
+    else:
+        exit()
+
+    process_args = {}
+    for e in lcu_process.cmdline():
+        if "=" in e:
+            key, value = e[2:].split("=", 1)
+            process_args.update({key: value})
+    return process_args
+
+
 class RiotClientApi:
-    lcu_process = None
-
     def __init__(self) -> None:
-        for e in process_iter():
-            if e.name() == "LeagueClientUx.exe":
-                 self.lcu_process = e
-        
-        self.get_auth()
-
-    def request(self, method, endpoint):
-        # proxies = {'https': ''}
-        return requests.request(method=method, url=f"https://127.0.0.1:{self.port}{endpoint}", verify=False, auth=self.auth)
-
-    def get_auth(self):
-        process_args = {}
-        for e in self.lcu_process.cmdline():
-            if '=' in e:
-                key, value = e[2:].split('=', 1)
-                process_args.update({key: value})
-
+        process_args = get_args()
         self.region = process_args.get("region")
         self.port = process_args.get("riotclient-app-port")
         self.auth = HTTPBasicAuth("riot", process_args.get("riotclient-auth-token"))
 
+    def request(self, method, endpoint):
+        return requests.request(
+            method=method, url=f"https://127.0.0.1:{self.port}{endpoint}", verify=False, auth=self.auth
+        )
 
-    
 
+class LcuApi:
+    def __init__(self) -> None:
+        process_args = get_args()
+        self.region = process_args.get("region")
+        self.port = process_args.get("app-port")
+        self.auth = HTTPBasicAuth("riot", process_args.get("remoting-auth-token"))
 
+    def request(self, method, endpoint):
+        return requests.request(
+            method=method, url=f"https://127.0.0.1:{self.port}{endpoint}", verify=False, auth=self.auth
+        )
--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-from api import RiotClientApi
-import webbrowser
+import PySimpleGUI as sg
+import funcs
 
-api = RiotClientApi()
-r = api.request("GET", "/chat/v5/participants/champ-select")
-if r.status_code != 200:
-    exit()
-r = r.json()
-if r["participants"]:
-    participants = []
-    for e in r["participants"]:
-        participants.append(e["name"])
-    url = (f"https://porofessor.gg/pregame/{api.region.lower()}/{','.join(participants)}")
-    webbrowser.open(url)
-else:
-    print("No players found!")
+layout = [
+    [sg.Text("Current team:", font=("System"), background_color="gray11", text_color="gray97")],
+    [
+        sg.Multiline(
+            "",
+            disabled=True,
+            no_scrollbar=True,
+            key="-chat-",
+            size=(20, 5),
+            font=("System", 17),
+            background_color="gray8",
+            text_color="gray97",
+        )
+    ],
+    [
+        sg.Button("Porofessor", enable_events=True, key="-search-", font=("System")),
+        sg.Button("Dodge", enable_events=True, key="-dodge-", font=("System")),
+    ],
+]
+
+window = sg.Window("View lobby names", layout, background_color="gray11", button_color="gray8")
+
+while True:
+    event, values = window.read()
+    match event:
+        case "-search-":
+            participants = funcs.search()
+            window["-chat-"].update(participants)
+        case "-dodge-":
+            if funcs.match_phase("ChampSelect"):
+                event, v = sg.Window(
+                    f"Confirm dodge?",
+                    [
+                        [
+                            sg.Stretch(background_color="gray11"),
+                            sg.Text("Penalties still apply!", font=("System"), background_color="gray11"),
+                            sg.Stretch(background_color="gray11"),
+                        ],
+                        [
+                            sg.Button("Yes", s=12, button_color=("gray97", "#731a1a"), font="System"),
+                            sg.Button("No", s=12, button_color=("gray97", "gray8"), font="System"),
+                        ],
+                    ],
+                    modal=True,
+                    background_color="gray11",
+                ).read(close=True)
+                if event == "Yes":
+                    funcs.dodge()
+        case sg.WIN_CLOSED:
+            break
