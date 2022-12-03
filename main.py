72ea130b57085e29a23c73c5fc48043a31f0121f
Vondy <gilenogong@hotmail.com>
2022-12-03 13:58:42 -0300

    Added boost feature

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import PySimpleGUI as sg
-import funcs
+import reveal
+import aram
 
-layout = [
+reveal_tab = [
     [sg.Text("Current team:", font=("System"), background_color="gray11", text_color="gray97")],
     [
         sg.Multiline(
@@ -21,16 +22,53 @@ layout = [
     ],
 ]
 
-window = sg.Window("View lobby names", layout, background_color="gray11", button_color="gray8")
+aram_tab = [
+    [sg.Text("State:", font=("System"), background_color="gray11", text_color="gray97")],
+    [        
+        sg.Multiline(
+            "",
+            disabled=True,
+            no_scrollbar=True,
+            key="-state-",
+            size=(20, 5),
+            font=("System", 17),
+            background_color="gray8",
+            text_color="gray97",
+        )
+    ],
+    [sg.Button("Boost", enable_events=True, key="-boost-", font="System"),
+    sg.Button("Refresh JWT", enable_events=True, key="-jwt-", font="System")],
+]
+
+layout = [
+    [
+        sg.TabGroup(
+            [
+                [sg.Tab("Reveal", reveal_tab, background_color="gray11")],
+                [sg.Tab("ARAM", aram_tab, background_color="gray11")],
+                [sg.Tab("Refund", [], background_color="gray11")],
+            ],
+            background_color="gray11",
+            tab_background_color="gray11",
+            focus_color="gray11",
+            title_color="gray50",
+            selected_title_color="gray97",
+            selected_background_color="gray11",
+        )
+    ]
+]
+
+window = sg.Window("LCU Util", layout, background_color="gray11", button_color="gray8")
 
 while True:
     event, values = window.read()
     match event:
         case "-search-":
-            participants = funcs.search()
+            participants = reveal.get_names()
+            reveal.search()
             window["-chat-"].update(participants)
         case "-dodge-":
-            if funcs.match_phase("ChampSelect"):
+            if reveal.match_phase("ChampSelect"):
                 event, v = sg.Window(
                     f"Confirm dodge?",
                     [
@@ -48,6 +86,10 @@ while True:
                     background_color="gray11",
                 ).read(close=True)
                 if event == "Yes":
-                    funcs.dodge()
+                    reveal.dodge()
+        case "-boost-":
+            aram.boost()
+        case "-jwt-":
+            aram.get_jwt()
         case sg.WIN_CLOSED:
             break
---
+++ reveal.py
@@ -0,0 +1,56 @@
+# reveal.py
+
+from apis import *
+import webbrowser
+import logging
+
+logging.basicConfig(
+    filename="auto.log",
+    level=logging.INFO,
+    format='%(asctime)s::"%(message)s"',
+)
+
+
+class RevealNames:
+    lcu_api = LcuApi()
+    riot_api = RiotClientApi()
+
+    def match_phase(self, phase) -> bool:
+        r = self.lcu_api.request("GET", "/lol-gameflow/v1/gameflow-phase")
+        if r.status_code != 200:
+            return
+        r = r.json()
+        return True if r == phase else False
+
+    def dodge(self) -> None:
+        r = self.lcu_api.request(
+            "post",
+            '/lol-login/v1/session/invoke?destination=lcdsServiceProxy&method=call&args=["","teambuilder-draft","quitV2",""]',
+        )
+        if r.status_code != 200:
+            pass
+
+    def get_names(self) -> str | list:
+        r = self.riot_api.request("GET", "/chat/v5/participants/champ-select")
+        if r.status_code != 200:
+            exit()
+        r = r.json()
+        participants = []
+        for e in r["participants"]:
+            participants.append(e["name"])
+        return participants if participants else "No players found!"
+
+    def search(self) -> None:
+        participants = self.get_names()
+        if isinstance(participants, str) or len(participants) == 1:
+            return
+        logging.info(str(participants))
+        url = f"https://porofessor.gg/pregame/{self.lcu_api.region.lower()}/{','.join(participants)}"
+        webbrowser.open(url)
+
+
+__instance = RevealNames()
+match_phase = __instance.match_phase
+dodge = __instance.dodge
+get_names = __instance.get_names
+search = __instance.search
